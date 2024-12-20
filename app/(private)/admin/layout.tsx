20bde255bb2c71d4cf1294c85fb6bbda43308c18
NNakreSS <serkanatmaca15@gmail.com>
2024-12-20 18:47:18 +0300

    feat: added session check

--- app/(private)/admin/layout.tsx
+++ app/(private)/admin/layout.tsx
@@ -1,12 +1,23 @@
-import Main from '@/components/Main';
-import { ThemeProvider } from '@/components/providers/theme-provider';
-import React from 'react';
+import Main from "@/components/Main";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { PATH_AUTH_LOGIN, PATH_DASHBOARD } from "@/constants/paths";
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
+import React from "react";
 
 export default async function PrivateLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await auth();
+
+  if (!session) {
+    return redirect(PATH_AUTH_LOGIN);
+  } else if (!session.user.isAdmin) {
+    return redirect(PATH_DASHBOARD);
+  }
+
   return (
     <>
       <ThemeProvider
