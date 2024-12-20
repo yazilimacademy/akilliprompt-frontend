0ead8b563e125bfe6d05dcf7a2fb3ab803f89e41
NNakreSS <serkanatmaca15@gmail.com>
2024-12-20 18:49:59 +0300

    feat: added login check

--- app/(private)/layout.tsx
+++ app/(private)/layout.tsx
@@ -1,18 +1,24 @@
-import AdminNavbar from '@/components/admin/navbar';
-import Main from '@/components/Main';
-import React from 'react';
+import AdminNavbar from "@/components/admin/navbar";
+import Main from "@/components/Main";
+import React from "react";
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
 export default async function PrivateLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await auth();
+
+  if (!session?.user) {
+    redirect("/login");
+  }
+
   return (
-    <>
-      <Main>
-        <AdminNavbar />
-        {children}
-      </Main>
-    </>
+    <Main>
+      <AdminNavbar />
+      {children}
+    </Main>
   );
 }
