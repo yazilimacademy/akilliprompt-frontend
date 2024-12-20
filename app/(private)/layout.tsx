f5f711670f8478dce371cc16480dac831b9e5bd9
NNakreSS <serkanatmaca15@gmail.com>
2024-12-20 16:00:51 +0300

    refactor: deleted unused import (EmailVerifiCation)

--- app/(private)/layout.tsx
+++ app/(private)/layout.tsx
@@ -1,13 +1,14 @@
-import AdminNavbar from '@/components/admin/navbar';
-import Main from '@/components/Main';
-import VerifyEmailNotice from '@/components/VerifyEmailNotice';
-import React from 'react';
+import AdminNavbar from "@/components/admin/navbar";
+import Main from "@/components/Main";
+import React from "react";
 
-export default async function PrivateLayout({ children }: { children: React.ReactNode }) {
-
+export default async function PrivateLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <>
-      <VerifyEmailNotice />
       <Main>
         <AdminNavbar />
         {children}
--- app/(public)/dashboard/page.tsx
+++ app/(public)/dashboard/page.tsx
@@ -1,17 +1,15 @@
-'use client';
+"use client";
 
-import Main from '@/components/Main';
-import {Button} from '@/components/ui/button';
-import {signOut, useSession} from 'next-auth/react';
-import Image from 'next/image';
-import VerifyEmailNotice from '@/components/VerifyEmailNotice';
-import React from 'react';
+import Main from "@/components/Main";
+import { Button } from "@/components/ui/button";
+import { signOut, useSession } from "next-auth/react";
+import React from "react";
 
 // TODO: giriş yapmamış üyelerin ücretsiz kısımlara erişip erişemeyeceği konuşulacak.
 export default function DashboardPage() {
-  const {data: session, status} = useSession();
+  const { data: session, status } = useSession();
 
-  if (status === 'loading') {
+  if (status === "loading") {
     return (
       <Main className="w-screen h-screen grid place-content-center">
         <p>Yükleniyor...</p>
@@ -21,21 +19,16 @@ export default function DashboardPage() {
 
   return (
     <>
-      <VerifyEmailNotice/>
       <Main className="h-screen grid place-content-center">
         <h1>Dashboard Page</h1>
         <p>{session?.user?.email}</p>
-        <p>{session?.user?.firstName}</p>
-        <p>{session?.user?.lastName}</p>
         <p>{session?.user?.name}</p>
-        <Image src={session?.user?.image} width={50} height={50} alt="User Image"/>
-        <p> isAdmin : {session?.user?.isAdmin ? 'Admin' : 'User'}</p>
         <Button
-          variant={session?.user ? 'destructive' : 'ghost'}
+          variant={session?.user ? "destructive" : "ghost"}
           disabled={!session?.user}
           onClick={() => signOut()}
         >
-                    Çıkış Yap
+          Çıkış Yap
         </Button>
       </Main>
     </>
